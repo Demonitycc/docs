d2554119da5560bdd61e42c408b3b3794b2e5d47
Demonitycc <lolsavvy418@gmail.com>
2025-03-08 22:06:21 -0500

    Create Prediction.lua

---
+++ Prediction.lua
@@ -0,0 +1,13 @@
+getgenv().Predict = function(TargetPosition, Velocity, ProjectileSpeed, ProjectileDrop, Continuous)
+    local PSpeed, PDrop = ProjectileSpeed or GetProjectileInfo()
+    if not PSpeed or not PDrop then return Vector3.zero, nil end
+    local function PredictOnce()
+        local Dist = (TargetPosition - Camera.CFrame.Position).Magnitude
+        if Dist <= 0 then return Vector3.zero, nil end
+        local TimeToHit = Dist / PSpeed
+        local PredictedPosition = TargetPosition + (Velocity or Vector3.zero) * TimeToHit
+        PredictedPosition -= Vector3.new(0, -PDrop ^ (TimeToHit * PDrop) + 1.5, 0)
+        return PredictedPosition, TimeToHit
+    end
+    return Continuous and RunService.Stepped:Wait() and PredictOnce() or PredictOnce()
+end
