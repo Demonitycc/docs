b19990d09bdbed17f5d997d19fba2b7449fcc3f0
swimhub <135133943+SWIMHUBISWIMMING@users.noreply.github.com>
2025-03-12 21:31:44 +0900

    better logic

--- Prediction.lua
+++ Prediction.lua
@@ -5,8 +5,8 @@ getgenv().Predict = function(TargetPosition, Velocity, ProjectileSpeed, Projecti
         local Dist = (TargetPosition - Camera.CFrame.Position).Magnitude
         if Dist <= 0 then return Vector3.zero, nil end
         local TimeToHit = Dist / PSpeed
-        local PredictedPosition = TargetPosition + (Velocity or Vector3.zero) * TimeToHit
-        PredictedPosition -= Vector3.new(0, -PDrop ^ (TimeToHit * PDrop) + 1.5, 0)
+        local PredictedPosition = TargetPosition + ((Velocity or Vector3.zero) / 0.15) * TimeToHit
+        PredictedPosition += Vector3.new(0, PDrop ^ (TimeToHit * PDrop) - 1, 0)
         return PredictedPosition, TimeToHit
     end
     return Continuous and RunService.Stepped:Wait() and PredictOnce() or PredictOnce()
